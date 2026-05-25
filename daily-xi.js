85e707b730043d3ad0a8367dfa4fca4e8ea7cae9
benbarrios87 <benbarrios87@gmail.com>
2026-05-25 22:52:14 +0200

    Update daily-xi.js

--- daily-xi.js
+++ daily-xi.js
@@ -1,1 +1,29 @@
+const challenge = {
+  title: "Lyn vs Fram",
+  date: "11.11.2023",
+  result: "10-1",
+  formation: "4-3-3",
 
+  lineup: [
+    "Alexander Pedersen",
+    "Herman Solberg Nilsen",
+    "Schneider",
+    "William Sell",
+    "Meinseth",
+    "Even Bydal",
+    "Henrik Elvevold",
+    "Kristiansen",
+    "Ole Breistøl",
+    "Hellum",
+    "Olsen"
+  ],
+
+  scorers: [
+    "Kristiansen",
+    "Loholt",
+    "Hellum",
+    "Elvevold",
+    "Laajab",
+    "Olsen"
+  ]
+};
