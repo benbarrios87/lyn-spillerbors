0cf69ff3e2f428e99c2d609d36b339d8157a6e7f
benbarrios87 <benbarrios87@gmail.com>
2026-05-26 16:50:59 +0200

    Update career-data.js

--- career-data.js
+++ career-data.js
@@ -142,15 +142,266 @@ const careers = [
     ]
   },
 
-  {
-    answer:"Tommy Berntsen",
-    clubs:[
-      { years:"1993–1998", club:"Skeid", apps:109 },
-      { years:"1998–2001", club:"Watford", apps:16 },
-      { years:"2001–2002", club:"Bryne", apps:37 },
-      { years:"2003–2008", club:"Lyn", apps:99 },
-      { years:"2009", club:"Manglerud Star", apps:11 }
-    ]
-  }
+ {
+  answer:"Tommy Berntsen",
+  clubs:[
+    { years:"1991–1994", club:"Vålerenga", apps:null },
+    { years:"1995", club:"Lørenskog", apps:null },
+    { years:"1996–1997", club:"Skjetten", apps:null },
+    { years:"1998–2000", club:"Lillestrøm", apps:72 },
+    { years:"1999", club:"Portsmouth (loan)", apps:2 },
+    { years:"2001", club:"Eintracht Frankfurt", apps:3 },
+    { years:"2002–2009", club:"Lyn", apps:null }
+  ]
+  },
+
+{
+  answer:"Ola Dybwad Olsen",
+  clubs:[
+    { years:"1964–1978", club:"Lyn", apps:219 },
+    { years:"1979–1982", club:"Stabæk", apps:null }
+  ]
+},
+
+{
+  answer:"Matías Almeyda",
+  clubs:[
+    { years:"1991–1996", club:"River Plate", apps:68 },
+    { years:"1996–1997", club:"Sevilla", apps:28 },
+    { years:"1997–2000", club:"Lazio", apps:63 },
+    { years:"2000–2002", club:"Parma", apps:34 },
+    { years:"2002–2004", club:"Inter Milan", apps:27 },
+    { years:"2004–2005", club:"Brescia", apps:5 },
+    { years:"2005", club:"Quilmes", apps:10 },
+    { years:"2007", club:"Lyn", apps:2 },
+    { years:"2009", club:"Fénix", apps:4 },
+    { years:"2009–2011", club:"River Plate", apps:62 }
+  ]
+},
+
+{
+  answer:"Per Egil Swift",
+  clubs:[
+    { years:"1990–1992", club:"Eik-Tønsberg", apps:null },
+    { years:"1993", club:"Start", apps:18 },
+    { years:"1994–1997", club:"Tromsø", apps:82 },
+    { years:"1998–2003", club:"Lyn", apps:145 },
+    { years:"2004", club:"Vålerenga", apps:17 },
+    { years:"2005", club:"FK Tønsberg", apps:16 }
+  ]
+},
+
+{
+  answer:"Ezekiel Bala",
+  clubs:[
+    { years:"2005–2007", club:"Lyn", apps:18 },
+    { years:"2008–2010", club:"Bryne", apps:74 },
+    { years:"2011", club:"Randaberg", apps:23 },
+    { years:"2012–2013", club:"Nybergsund", apps:null },
+    { years:"2014", club:"Funnefoss/Vormsund", apps:null },
+    { years:"2015–2018", club:"Ålgård", apps:null },
+    { years:"2019", club:"Rosseland", apps:null }
+  ]
+},
+
+{
+  answer:"Harald Berg",
+  clubs:[
+    { years:"1958–1964", club:"Bodø/Glimt", apps:90 },
+    { years:"1965–1969", club:"Lyn", apps:98 },
+    { years:"1969–1974", club:"ADO Den Haag", apps:129 },
+    { years:"1974–1981", club:"Bodø/Glimt", apps:135 }
+  ]
+},
+
+{
+  answer:"Jo Inge Berget",
+  clubs:[
+    { years:"2008", club:"Lyn", apps:2 },
+    { years:"2008–2011", club:"Udinese", apps:0 },
+    { years:"2009", club:"Lyn (loan)", apps:27 },
+    { years:"2010–2011", club:"Strømsgodset (loan)", apps:31 },
+    { years:"2011–2014", club:"Molde", apps:63 },
+    { years:"2014–2015", club:"Cardiff City", apps:1 },
+    { years:"2014", club:"Celtic (loan)", apps:4 },
+    { years:"2015–2017", club:"Malmö FF", apps:77 },
+    { years:"2018", club:"New York City", apps:23 },
+    { years:"2019–2022", club:"Malmö FF", apps:96 },
+    { years:"2023–", club:"Sarpsborg 08", apps:38 }
+  ]
+},
+
+{
+  answer:"Lars Bohinen",
+  clubs:[
+    { years:"1986", club:"Bærum", apps:2 },
+    { years:"1987", club:"Lyn", apps:20 },
+    { years:"1988–1989", club:"Vålerenga", apps:33 },
+    { years:"1990", club:"Viking", apps:10 },
+    { years:"1990–1993", club:"Young Boys", apps:58 },
+    { years:"1993", club:"Lillestrøm (loan)", apps:19 },
+    { years:"1993–1995", club:"Nottingham Forest", apps:64 },
+    { years:"1995–1998", club:"Blackburn Rovers", apps:58 },
+    { years:"1998–2001", club:"Derby County", apps:56 },
+    { years:"2001–2002", club:"Lyngby", apps:26 },
+    { years:"2002", club:"Farum", apps:2 },
+    { years:"2003", club:"Vålerenga", apps:6 }
+  ]
+},
+
+{
+  answer:"Johan Dahlin",
+  clubs:[
+    { years:"2003", club:"Trollhättan", apps:1 },
+    { years:"2004", club:"Åsebro", apps:18 },
+    { years:"2005–2009", club:"Lyn", apps:29 },
+    { years:"2008", club:"Trelleborg (loan)", apps:14 },
+    { years:"2009–2013", club:"Malmö FF", apps:104 },
+    { years:"2014–2015", club:"Gençlerbirliği", apps:20 },
+    { years:"2015–2017", club:"Midtjylland", apps:39 },
+    { years:"2017–", club:"Malmö FF", apps:166 }
+  ]
+},
+
+{
+  answer:"Hassan El Fakiri",
+  clubs:[
+    { years:"1995–1999", club:"Lyn", apps:103 },
+    { years:"2000", club:"Brann", apps:9 },
+    { years:"2000–2005", club:"Monaco", apps:55 },
+    { years:"2001", club:"Lyn (loan)", apps:13 },
+    { years:"2001–2002", club:"Rosenborg (loan)", apps:20 },
+    { years:"2005–2007", club:"Gladbach", apps:52 },
+    { years:"2007–2014", club:"Brann", apps:187 }
+  ]
+},
+
+{
+  answer:"Henrik Elvevold",
+  clubs:[
+    { years:"2019", club:"Lokomotiv Oslo", apps:24 },
+    { years:"2020–2023", club:"Lyn", apps:65 },
+    { years:"2024–2025", club:"Egersund", apps:22 },
+    { years:"2025–", club:"Hønefoss", apps:"?" }
+  ]
+},
+
+{
+  answer:"Stefan Gíslason",
+  clubs:[
+    { years:"1999–2001", club:"Strømsgodset", apps:75 },
+    { years:"2002–2003", club:"Grazer AK", apps:null },
+    { years:"2003–2005", club:"Keflavík", apps:null },
+    { years:"2005–2007", club:"Lyn", apps:80 },
+    { years:"2007–2011", club:"Brøndby", apps:70 },
+    { years:"2010–2011", club:"Viking (loan)", apps:12 },
+    { years:"2011", club:"Lillestrøm", apps:15 },
+    { years:"2012–2014", club:"OH Leuven", apps:61 },
+    { years:"2014", club:"Breiðablik", apps:null }
+  ]
+},
+
+{
+  answer:"Fredrik Gulbrandsen",
+  clubs:[
+    { years:"2008–2013", club:"Lillestrøm", apps:57 },
+    { years:"2010", club:"Lyn (loan)", apps:8 },
+    { years:"2013–2016", club:"Molde", apps:42 },
+    { years:"2016–2019", club:"Red Bull Salzburg", apps:62 },
+    { years:"2017", club:"New York Red Bulls (loan)", apps:12 },
+    { years:"2019–2022", club:"İstanbul Başakşehir", apps:79 },
+    { years:"2022–2023", club:"Adana Demirspor", apps:23 },
+    { years:"2023–", club:"Molde", apps:8 }
+  ]
+},
+
+{
+  answer:"Eddie Gustafsson",
+  clubs:[
+    { years:"1994", club:"IFK Stockholm", apps:4 },
+    { years:"1995–2001", club:"IFK Norrköping", apps:86 },
+    { years:"2002–2004", club:"Molde", apps:71 },
+    { years:"2005", club:"HamKam", apps:26 },
+    { years:"2005–2008", club:"Lyn", apps:63 },
+    { years:"2009–2014", club:"Red Bull Salzburg", apps:69 }
+  ]
+},
+
+{
+  answer:"Glenn Hartmann",
+  clubs:[
+    { years:"1991–1994", club:"Lyn", apps:20 },
+    { years:"1993", club:"Bærum (loan)", apps:null },
+    { years:"1995", club:"Fossum (loan)", apps:null },
+    { years:"1996", club:"Ullern", apps:null },
+    { years:"1997–2000", club:"Aalesund", apps:50 },
+    { years:"2002", club:"Hødd", apps:27 },
+    { years:"2003", club:"Skarbøvik", apps:null },
+    { years:"2004", club:"Hødd", apps:22 }
+  ]
+},
+
+{
+  answer:"Espen Hoff",
+  clubs:[
+    { years:"1999–2006", club:"Odd Grenland", apps:154 },
+    { years:"2006–2008", club:"Lyn", apps:67 },
+    { years:"2009–2010", club:"Stabæk", apps:45 },
+    { years:"2010–2016", club:"Start", apps:160 }
+  ]
+},
+
+{
+  answer:"Kim Holmen",
+  clubs:[
+    { years:"2006", club:"Lørenskog", apps:71 },
+    { years:"2007–2009", club:"Lyn", apps:56 },
+    { years:"2010–2011", club:"Kongsvinger", apps:28 },
+    { years:"2012", club:"Ullensaker/Kisa", apps:5 },
+    { years:"2014", club:"Lyn", apps:null }
+  ]
+},
+
+{
+  answer:"Steven Lustü",
+  clubs:[
+    { years:"1991–1993", club:"Næstved BK", apps:19 },
+    { years:"1993–2000", club:"HB Køge", apps:169 },
+    { years:"2000–2001", club:"AB", apps:41 },
+    { years:"2002–2006", club:"Lyn", apps:106 },
+    { years:"2007–2009", club:"Silkeborg IF", apps:60 }
+  ]
+},
+
+{
+  answer:"Erling Knudtzon",
+  clubs:[
+    { years:"2004–2006", club:"Ullern", apps:0 },
+    { years:"2007–2010", club:"Lyn", apps:66 },
+    { years:"2010–2018", club:"Lillestrøm", apps:227 },
+    { years:"2019–2023", club:"Molde", apps:113 },
+    { years:"2024", club:"Lillestrøm", apps:11 }
+  ]
+},
+
+{
+  answer:"Edwin Kjelder",
+  clubs:[
+    { years:"2006", club:"Ålgård", apps:0 },
+    { years:"2006–2010", club:"Lyn", apps:6 },
+    { years:"2010–2011", club:"Ålgård", apps:36 },
+    { years:"2012", club:"Lyn", apps:23 },
+    { years:"2013–2014", club:"Ålgård", apps:0 }
+  ]
+},
+
+{
+  answer:"Mounir Hamoud",
+  clubs:[
+    { years:"2004–2005", club:"Lyn", apps:36 },
+    { years:"2006–2011", club:"Bodø/Glimt", apps:160 },
+    { years:"2012–2019", club:"Strømsgodset", apps:112 }
+  ]
+}
 
 ];
