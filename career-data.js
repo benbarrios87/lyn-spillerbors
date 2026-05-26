33079f884445f1b28d76c63fccd155578854ee1d
benbarrios87 <benbarrios87@gmail.com>
2026-05-26 16:09:33 +0200

    Update career-data.js

--- career-data.js
+++ career-data.js
@@ -1,1 +1,156 @@
+const careers = [
 
+  {
+    answer:"Kent Bergersen",
+    clubs:[
+      { years:"1986–1990", club:"Drøbak-Frogn", apps:null },
+      { years:"1991–1992", club:"Lyn", apps:40 },
+      { years:"1993–1994", club:"Rosenborg", apps:43 },
+      { years:"1995–1996", club:"Vålerenga", apps:44 },
+      { years:"1996", club:"Raith Rovers", apps:6 },
+      { years:"1997–1999", club:"Panionios", apps:null },
+      { years:"1999", club:"Strømsgodset", apps:8 },
+      { years:"1999–2001", club:"Stockport County", apps:26 },
+      { years:"2000", club:"Moss (loan)", apps:13 },
+      { years:"2001–2002", club:"Moss", apps:25 },
+      { years:"2002", club:"Fredrikstad", apps:12 },
+      { years:"2003", club:"Drøbak-Frogn", apps:null }
+    ]
+  },
+
+  {
+    answer:"Jan Derek Sørensen",
+    clubs:[
+      { years:"1991–1994", club:"Lyn", apps:49 },
+      { years:"1995–1997", club:"Bodø/Glimt", apps:69 },
+      { years:"1998–2000", club:"Rosenborg", apps:68 },
+      { years:"2000–2003", club:"Borussia Dortmund", apps:24 },
+      { years:"2003–2005", club:"Lyn", apps:69 },
+      { years:"2006–2007", club:"Vålerenga", apps:41 },
+      { years:"2008–2009", club:"Bodø/Glimt", apps:44 }
+    ]
+  },
+
+  {
+    answer:"Ali Al-Habsi",
+    clubs:[
+      { years:"1998–2002", club:"Al-Mudhaibi", apps:35 },
+      { years:"2002–2003", club:"Al-Nasr", apps:4 },
+      { years:"2003–2006", club:"Lyn Oslo", apps:62 },
+      { years:"2006–2011", club:"Bolton Wanderers", apps:10 },
+      { years:"2010–2011", club:"Wigan Athletic (loan)", apps:34 },
+      { years:"2011–2015", club:"Wigan Athletic", apps:102 },
+      { years:"2014", club:"Brighton (loan)", apps:1 },
+      { years:"2015–2017", club:"Reading", apps:78 },
+      { years:"2017–2019", club:"Al-Hilal", apps:34 },
+      { years:"2019–2020", club:"West Bromwich", apps:0 }
+    ]
+  },
+
+  {
+    answer:"Ibba Laajab",
+    clubs:[
+      { years:"2003–2005", club:"Skeid", apps:18 },
+      { years:"2005", club:"Drøbak-Frogn (loan)", apps:null },
+      { years:"2006", club:"Sørumsand", apps:0 },
+      { years:"2006–2007", club:"Gladbach II", apps:1 },
+      { years:"2007", club:"Skeid", apps:16 },
+      { years:"2008–2009", club:"Notodden", apps:43 },
+      { years:"2010", club:"Mjøndalen", apps:27 },
+      { years:"2011", club:"Strømmen", apps:24 },
+      { years:"2012", club:"Vålerenga", apps:6 },
+      { years:"2012", club:"Stabæk (loan)", apps:5 },
+      { years:"2013–2014", club:"Bodø/Glimt", apps:56 },
+      { years:"2015", club:"Hebei China Fortune", apps:30 },
+      { years:"2016–2020", club:"Yokohama FC", apps:156 },
+      { years:"2020–2022", club:"Omiya Ardija", apps:35 },
+      { years:"2022–2024", club:"Lyn", apps:35 }
+    ]
+  },
+
+  {
+    answer:"Erik Botheim",
+    clubs:[
+      { years:"2015–2016", club:"Lyn", apps:18 },
+      { years:"2017–2020", club:"Rosenborg", apps:19 },
+      { years:"2020", club:"Stabæk (loan)", apps:15 },
+      { years:"2021", club:"Bodø/Glimt", apps:30 },
+      { years:"2022", club:"Krasnodar", apps:0 },
+      { years:"2022–2024", club:"Salernitana", apps:35 },
+      { years:"2024–", club:"Malmö FF", apps:45 }
+    ]
+  },
+
+  {
+    answer:"Ronny Johnsen",
+    clubs:[
+      { years:"1985–1986", club:"Stokke", apps:null },
+      { years:"1987–1991", club:"Eik-Tønsberg", apps:null },
+      { years:"1992–1994", club:"Lyn", apps:38 },
+      { years:"1994–1995", club:"Lillestrøm", apps:23 },
+      { years:"1995–1996", club:"Beşiktaş", apps:22 },
+      { years:"1996–2002", club:"Manchester United", apps:99 },
+      { years:"2002–2004", club:"Aston Villa", apps:49 },
+      { years:"2004", club:"Newcastle", apps:3 },
+      { years:"2005–2008", club:"Vålerenga", apps:65 }
+    ]
+  },
+
+  {
+    answer:"Odion Ighalo",
+    clubs:[
+      { years:"2005–2006", club:"Prime", apps:5 },
+      { years:"2006–2007", club:"Julius Berger", apps:10 },
+      { years:"2007–2008", club:"Lyn", apps:20 },
+      { years:"2008–2014", club:"Udinese", apps:6 },
+      { years:"2009–2010", club:"Granada (loan)", apps:26 },
+      { years:"2010–2011", club:"Cesena (loan)", apps:3 },
+      { years:"2011–2014", club:"Granada (loan)", apps:95 },
+      { years:"2014", club:"Watford (loan)", apps:8 },
+      { years:"2014–2017", club:"Watford", apps:82 },
+      { years:"2017–2018", club:"Changchun Yatai", apps:55 },
+      { years:"2019–2021", club:"Shanghai Greenland", apps:17 },
+      { years:"2020–2021", club:"Manchester United (loan)", apps:12 },
+      { years:"2021–", club:"Al-Shabab", apps:4 }
+    ]
+  },
+
+  {
+    answer:"Andreas Hellum",
+    clubs:[
+      { years:"2016–2020", club:"Mjøndalen", apps:56 },
+      { years:"2016", club:"Asker (loan)", apps:9 },
+      { years:"2018", club:"Nybergsund (loan)", apps:10 },
+      { years:"2019", club:"Strømmen (loan)", apps:18 },
+      { years:"2021–2022", club:"Arendal", apps:36 },
+      { years:"2023–", club:"Lyn", apps:26 }
+    ]
+  },
+
+  {
+    answer:"Øyvind Leonhardsen",
+    clubs:[
+      { years:"1988", club:"Clausenengen", apps:"?" },
+      { years:"1989–1991", club:"Molde", apps:64 },
+      { years:"1992–1994", club:"Rosenborg", apps:63 },
+      { years:"1994–1997", club:"Wimbledon", apps:76 },
+      { years:"1997–1999", club:"Liverpool", apps:37 },
+      { years:"1999–2002", club:"Tottenham", apps:54 },
+      { years:"2002–2003", club:"Aston Villa", apps:19 },
+      { years:"2004–2005", club:"Lyn", apps:46 },
+      { years:"2006–2007", club:"Strømsgodset", apps:43 }
+    ]
+  },
+
+  {
+    answer:"Tommy Berntsen",
+    clubs:[
+      { years:"1993–1998", club:"Skeid", apps:109 },
+      { years:"1998–2001", club:"Watford", apps:16 },
+      { years:"2001–2002", club:"Bryne", apps:37 },
+      { years:"2003–2008", club:"Lyn", apps:99 },
+      { years:"2009", club:"Manglerud Star", apps:11 }
+    ]
+  }
+
+];
