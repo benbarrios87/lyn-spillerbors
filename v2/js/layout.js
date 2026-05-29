f51b310481d2f419cd704ee1d9ca9c609b625891
benbarrios87 <benbarrios87@gmail.com>
2026-05-29 18:27:06 +0200

    Update layout.js

--- v2/js/layout.js
+++ v2/js/layout.js
@@ -6,26 +6,17 @@ export function renderNavbar(active = "") {
         <span>Lynbørs 2.0</span>
       </a>
 
-      <nav class="nav">
+     <nav class="nav">
   <a href="index.html" class="${active === "home" ? "active" : ""}">Hjem</a>
   <a href="kamper.html" class="${active === "kamper" ? "active" : ""}">Kamper</a>
-  <a href="bors.html" class="${active === "bors" ? "active" : ""}">Børs</a>
-  <a href="stats.html" class="${active === "stats" ? "active" : ""}">Stats</a>
   <a href="stem.html" class="${active === "stem" ? "active" : ""}">Stem</a>
+  <a href="bors.html" class="${active === "bors" ? "active" : ""}">Børs</a>
+  <a href="fantasy.html" class="${active === "fantasy" ? "active" : ""}">Fantasy</a>
+  <a href="tips.html" class="${active === "tips" ? "active" : ""}">Tips</a>
+  <a href="lynligaen.html" class="${active === "lynligaen" ? "active" : ""}">Lynligaen</a>
+  <a href="stats.html" class="${active === "stats" ? "active" : ""}">Stats</a>
 </nav>
 
-        <a href="kamper.html" class="${active === "kamper" ? "active" : ""}">
-          Kamper
-        </a>
-
-        <a href="#" class="disabled">
-          Børs
-        </a>
-
-        <a href="#" class="disabled">
-          Fantasy
-        </a>
-      </nav>
     </header>
   `;
 }
