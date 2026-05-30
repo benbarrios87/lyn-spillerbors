91bbe8129e5759fd429e0f9bda74a33e440daaa1
benbarrios87 <benbarrios87@gmail.com>
2026-05-30 12:11:33 +0200

    Create tenable-data.js

---
+++ tenable-data.js
@@ -0,0 +1,107 @@
+window.tenableChallenges = [
+  {
+    id: "lyn-redcards-001",
+    title: "Lyn-spillere siden 1990 med minst 2 røde kort",
+    intro: "Tallene viser antall røde kort. Klarer du å finne spillerne?",
+    answers: [
+      {
+        label: "Steven Lustü",
+        value: 4,
+        aliases: ["steven lustu", "lustu", "lüstü", "steven lüstü"]
+      },
+      {
+        label: "Mads Dahm",
+        value: 3,
+        aliases: ["mads dahm", "dahm"]
+      },
+      {
+        label: "Christoffer Lars Simensen",
+        value: 2,
+        aliases: ["christoffer lars simensen", "christoffer simensen", "simensen"]
+      },
+      {
+        label: "Eirik Haugstad",
+        value: 2,
+        aliases: ["eirik haugstad", "haugstad"]
+      },
+      {
+        label: "Kristian Flittie Onstad",
+        value: 2,
+        aliases: ["kristian flittie onstad", "flittie onstad", "onstad"]
+      },
+      {
+        label: "Mohammed Ouseb",
+        value: 2,
+        aliases: ["mohammed ouseb", "ouseb"]
+      },
+      {
+        label: "Paul Obiefule",
+        value: 2,
+        aliases: ["paul obiefule", "obiefule"]
+      },
+      {
+        label: "Theodor Elmar Bjarnasson",
+        value: 2,
+        aliases: ["theodor elmar bjarnasson", "theodor bjarnasson", "bjarnasson"]
+      }
+    ]
+  },
+
+  {
+    id: "lyn-nationalities-001",
+    title: "Nasjonaliteter i Lyns A-lag siden 1990 med minst 3 spillere",
+    intro: "Tallene viser antall spillere fra landet. Norge er ikke med.",
+    answers: [
+      {
+        label: "Sverige",
+        value: 16,
+        aliases: ["sverige", "svensk", "sweden"]
+      },
+      {
+        label: "Island",
+        value: 11,
+        aliases: ["island", "iceland"]
+      },
+      {
+        label: "Nigeria",
+        value: 7,
+        aliases: ["nigeria"]
+      },
+      {
+        label: "Australia",
+        value: 5,
+        aliases: ["australia"]
+      },
+      {
+        label: "Senegal",
+        value: 3,
+        aliases: ["senegal"]
+      },
+      {
+        label: "England",
+        value: 3,
+        aliases: ["england"]
+      },
+      {
+        label: "Danmark",
+        value: 3,
+        aliases: ["danmark", "denmark"]
+      },
+      {
+        label: "Elfenbenskysten",
+        value: 3,
+        aliases: ["elfenbenskysten", "ivory coast", "cote divoire", "côte d'ivoire"]
+      },
+      {
+        label: "Frankrike",
+        value: 3,
+        aliases: ["frankrike", "france"]
+      },
+      {
+        label: "Argentina",
+        value: 3,
+        aliases: ["argentina"]
+      }
+    ]
+  }
+];
